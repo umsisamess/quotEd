d7fc3f88ddaa366f3c673a3906956600de413790
unknown <umeshsaud2001@gmail.com>
2021-08-06 21:21:00 +0545

    question page created

--- lib/pages/question.dart
+++ lib/pages/question.dart
@@ -8,10 +8,128 @@ class Question extends StatefulWidget {
 }
 
 class _QuestionState extends State<Question> {
+
+  Map data = {};
+
   @override
   Widget build(BuildContext context) {
+
+    data = ModalRoute.of(context)?.settings.arguments as Map;
+    print(data);
+
     return Scaffold(
       backgroundColor: Colors.blue[500],
+      body: Align(
+        alignment: Alignment.topCenter,
+        child: Column(
+          children: [
+            Container(
+              width: double.infinity,
+              margin: EdgeInsets.fromLTRB(30, 100, 30, 0),
+              child: Text(
+                data['quote'],
+                style: TextStyle(
+                  fontSize: 20,
+                  color: Colors.white,
+                ),
+                textAlign: TextAlign.center,
+                
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+              width: double.infinity,
+              child: SizedBox(
+                height: 35,
+                child: ElevatedButton(
+                  child: Text(
+                    data['options'][0],
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+              width: double.infinity,
+              child: SizedBox(
+                height: 35,
+                child: ElevatedButton(
+                  child: Text(
+                    data['options'][1],
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+              width: double.infinity,
+              child: SizedBox(
+                height: 35,
+                child: ElevatedButton(
+                  child: Text(
+                    data['options'][2],
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            Container(
+              margin: EdgeInsets.fromLTRB(30, 30, 30, 0),
+              width: double.infinity,
+              child: SizedBox(
+                height: 35,
+                child: ElevatedButton(
+                  child: Text(
+                    data['options'][3],
+                    style: TextStyle(
+                      fontSize: 20,
+                    ),
+                  ),
+                  style: ButtonStyle(
+                    backgroundColor: MaterialStateProperty.all<Color>(Colors.grey),
+                  ),
+                  onPressed: (){},
+                ),
+              ),
+            ),
+            Container(
+             margin: EdgeInsets.fromLTRB(30, 50, 30, 0),
+             child: ElevatedButton(
+               child: Text(
+                 'Next',
+                 style: TextStyle(
+                   fontSize: 20,
+                 ),
+               ),
+               style: ButtonStyle(
+                 backgroundColor: MaterialStateProperty.all<Color>(Colors.red),
+               ),
+               onPressed: (){},
+             ), 
+            ),
+          ]
+        ),
+      ),
     );
   }
 }
